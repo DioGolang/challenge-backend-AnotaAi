caf4e0ebc38306e672497736be923f60d4ac3560
dvasconcelos <di0g0vasconcelos@hotmail.com>
2025-03-18 23:46:50 -0300

    :sparkles: feat:  create builder category

--- src/domain/build/category.builder.ts
+++ src/domain/build/category.builder.ts
@@ -1,1 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { EntityBuilder } from './entity.builder';
+import { Category } from '../entities/category.entity';
+import { CategoryProps } from '../interfaces/props/category-props.interface';
 
+@Injectable()
+export class CategoryBuilder extends EntityBuilder<Category> {
+  private _props: Partial<CategoryProps> = {};
+
+  setTitle(title: string): this {
+    this._props.title = title;
+    return this;
+  }
+
+  setDescription(description: string): this {
+    this._props.description = description;
+    return this;
+  }
+
+  setOwnerId(ownerId: string): this {
+    this._props.ownerId = ownerId;
+    return this;
+  }
+
+  async build(): Promise<Category> {
+    return new Category(this._id, this._props as CategoryProps);
+  }
+
+  protected validateRequiredProperties(): void {
+    throw new Error('Method not implemented.');
+  }
+  protected addRelationshipsToEntities(entity: Category): void {
+    throw new Error('Method not implemented.');
+  }
+}
